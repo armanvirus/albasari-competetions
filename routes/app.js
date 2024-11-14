2b2c8e4ab27c3aba3f65255dc85447cab34e9513
mufteem <mr.zplux009@gmail.com>
2024-11-14 20:48:58 +0100

    mufteem branching

--- routes/app.js
+++ routes/app.js
@@ -4,5 +4,9 @@ const router = express.Router();
 
 // define the routes
 router.get('/dashboard', (req,res)=> res.render('pages/dashboard',{error:false, msg:''})) 
+router.get('/application', (req,res)=> res.render('pages/application',{error:false, msg:''})) 
+router.get('/payment', (req,res)=> res.render('pages/payment',{error:false, msg:''})) 
+router.get('/document', (req,res)=> res.render('pages/document',{error:false, msg:''})) 
+router.get('/profile', (req,res)=> res.render('pages/profile',{error:false, msg:''}))
 
 module.exports = router;
