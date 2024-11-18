5cd5c52dc15fbf2d4f7d4600cc8bb08ed9f0bb32
Arman Grema <abdurrahmangrema19@gmail.com>
2024-11-18 08:02:58 -0800

    added real data for students list

--- routes/app.js
+++ routes/app.js
@@ -8,10 +8,10 @@ router.get('/dashboard', isAuthenticated, dashboard);
 router.get('/application', isAuthenticated, (req,res)=> res.render('pages/application',{error:false, msg:''})) 
 router.get('/hadith', isAuthenticated, (req,res)=> res.render('pages/hadith',{error:false, msg:''})) 
 router.get('/payment',isAuthenticated, (req,res)=> res.render('pages/payment',{error:false, msg:''})) 
-router.get('/document', isAuthenticated, (req,res)=> res.render('pages/document',{error:false, msg:''})) 
+// router.get('/document', isAuthenticated, (req,res)=> res.render('pages/document',{error:false, msg:''})) 
 router.get('/profile', isAuthenticated, (req,res)=> res.render('pages/profile',{error:false, msg:''}))
 router.post("/application", isAuthenticated, application)
-router.get("/participants",isAuthenticated, participants)
+router.get("/document",isAuthenticated, participants)
 router.post("/quiz",isAuthenticated, quiz)
 
 
