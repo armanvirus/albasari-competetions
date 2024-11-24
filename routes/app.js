45d0ba5759a140385cfdd16cfddb462ad2dbf277
mufteem <mr.zplux009@gmail.com>
2024-11-24 10:18:21 +0100

    admin dashboard UI init

--- routes/app.js
+++ routes/app.js
@@ -1,24 +1,26 @@
 const express = require("express")
 const router = express.Router();
-const {dashboard,application, participants,quiz,editApplication, edit, edith, edithApplication, pay} = require('../controllers/app')
-const {isAuthenticated} = require('../middlewares/authenticator')
+const { dashboard, application, participants, quiz, editApplication, edit, edith, edithApplication, pay } = require('../controllers/app')
+const { isAuthenticated } = require('../middlewares/authenticator')
 
 // define the routes
 router.get('/dashboard', isAuthenticated, dashboard);
-router.get('/application', isAuthenticated, (req,res)=> res.render('pages/application',{error:false, msg:''})) 
-router.get('/hadith', isAuthenticated, (req,res)=> res.render('pages/hadith',{error:false, msg:''})) 
-router.get('/pay',isAuthenticated, (req,res)=> res.render('pages/pay',{error:false, msg:''})) 
-router.get('/payment',isAuthenticated, (req,res)=> res.render('pages/payment',{error:false, msg:''})) 
-router.get('/application/edit/:id',isAuthenticated, editApplication) 
-router.get('/hadith/edit/:id',isAuthenticated, edithApplication) 
+router.get('/application', isAuthenticated, (req, res) => res.render('pages/application', { error: false, msg: '' }))
+router.get('/hadith', isAuthenticated, (req, res) => res.render('pages/hadith', { error: false, msg: '' }))
+router.get('/pay', isAuthenticated, (req, res) => res.render('pages/pay', { error: false, msg: '' }))
+// admin route
+router.get('/admin', (req, res) => res.render('pages/admin', { error: false, msg: '' }))
+router.get('/payment', isAuthenticated, (req, res) => res.render('pages/payment', { error: false, msg: '' }))
+router.get('/application/edit/:id', isAuthenticated, editApplication)
+router.get('/hadith/edit/:id', isAuthenticated, edithApplication)
 // router.get('/document', isAuthenticated, (req,res)=> res.render('pages/document',{error:false, msg:''})) 
-router.get('/profile', isAuthenticated, (req,res)=> res.render('pages/profile',{error:false, msg:'', user:req.user}))
+router.get('/profile', isAuthenticated, (req, res) => res.render('pages/profile', { error: false, msg: '', user: req.user }))
 router.post("/application", isAuthenticated, application)
-router.get("/document",isAuthenticated, participants)
-router.post("/quiz",isAuthenticated, quiz)
-router.post('/application/edit/:id',isAuthenticated, edit)
-router.post('/application/edith/:id',isAuthenticated, edith)
-router.post('/pay',isAuthenticated, pay)
+router.get("/document", isAuthenticated, participants)
+router.post("/quiz", isAuthenticated, quiz)
+router.post('/application/edit/:id', isAuthenticated, edit)
+router.post('/application/edith/:id', isAuthenticated, edith)
+router.post('/pay', isAuthenticated, pay)
 
 
 
